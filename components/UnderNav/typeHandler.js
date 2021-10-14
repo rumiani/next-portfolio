d69a316cc2eea88ea4e96f443926832aafb9b1ad
rumi <maziar9170@gmail.com>
2021-10-14 14:11:02 +0330

    fix the br for whoiam component and also for the project pic width

---
+++ components/UnderNav/typeHandler.js
@@ -0,0 +1,24 @@
+// import classes from '../../styles/UnderNav.module.css'
+
+// const typeHandler = (t) =>{
+//         console.log(t);
+//     if (typeof window === 'object' && t) {
+//             let getElement;
+//      getElement = document.querySelector(`.${classes.whoIAm}`)
+//     // let fullText = t.myNameIs
+//     let currentText = ``
+//     let i = 0;
+//     let interval = setInterval(()=>{
+//         currentText = currentText + t[i];
+//         getElement.innerHTML = currentText+'_';
+//         i++;
+//         if (i >= t.length){        
+//             clearInterval(interval);
+//             return getElement.innerHTML = t;
+//         }
+//     }, 50); 
+
+
+// }        
+// }
+// export default  typeHandler();
